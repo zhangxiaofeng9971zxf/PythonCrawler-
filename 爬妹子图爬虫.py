7715fab52c104dee2f8c4ff9f2ce1b2562b51e97
zhangxiaofeng9971zxf <zhangxiaofeng9971@126.com>
2019-01-27 14:41:02 +0800

    从html页面查找jpg文件，并且爬下来的爬虫 基于beautifulsoup编写

---
+++ 爬妹子图爬虫.py
@@ -0,0 +1,27 @@
+#从妹子图网上爬照片的爬虫v1.0
+
+import requests
+from bs4 import BeautifulSoup
+import time
+def download_page(url):
+   headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
+   r = requests.get(url, headers=headers)
+   r.encoding = 'gb2312'
+   return r.text
+def get_pic(pic_link):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
+    r = requests.get(pic_link, headers=headers)  # 下载图片，之后保存到文件
+    with open('pic{}'.format(pic_link.split('/')[-1]), 'wb') as f:
+           f.write(r.content)
+           time.sleep(0.1)
+
+
+url ='http://www.meizitu.com/a/5557.html'
+html=download_page(url)
+soup = BeautifulSoup(html, 'html.parser')
+pic_list = soup.find('div',class_="postContent").find_all('img')#找到所有用img开头的
+for i in pic_list:
+    pic_link = i.get('src')#或者src后的图片链接
+    get_pic(pic_link)
+
+
